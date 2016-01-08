1125673925dd9e778fe58d21cbaaa4b7d5204522
JJ Berry <JJBerry@gmail.com>
2016-01-08 15:00:10 -0800

    Adding a comment to view GitHub event via API listener on partner's machine

--- webhookTest.py
+++ webhookTest.py
@@ -2,6 +2,8 @@
 # By JJ Berry and Corey Gillen
 # Use this for whatever you want!
 
+# Adding a comment to view GitHub event via API listener on partner's machine
+
 import unittest
 from subprocess import call
 
