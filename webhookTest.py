0e771d4d597099079bdd68223e56cdc0fe0820ad
JJ Berry <JJBerry@gmail.com>
2016-01-08 15:02:21 -0800

    Removed comment to generate another push

--- webhookTest.py
+++ webhookTest.py
@@ -2,8 +2,6 @@
 # By JJ Berry and Corey Gillen
 # Use this for whatever you want!
 
-# Adding a comment to view GitHub event via API listener on partner's machine
-
 import unittest
 from subprocess import call
 
